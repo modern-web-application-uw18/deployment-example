3cc8ffa4c719af9ada588ce117fb22ed82e233bc
bhague1281 <bhague1281@gmail.com>
2018-06-11 19:22:09 -0700

    GH pages deployment + microservice call

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://microservice-template-dnxigvietj.now.sh/weather')
+      .then(response => response.json())
+      .then(data => {
+        this.setState(() => {
+          return {
+            weather: data.weather
+          };
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {JSON.stringify(this.state.weather)}
       </div>
     );
   }
